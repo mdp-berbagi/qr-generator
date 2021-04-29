b4f38583211f5992b8a01af2fe9431145c4228b4
Abdul Aziz Al Basyir <abdulazizalbasyir119@gmail.com>
2021-04-29 17:18:31 +0700

    Update serve.js

--- serve.js
+++ serve.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
+  // https://chart.googleapis.com/chart?cht=qr&chld=H|1&chs=[200x200]&chl=[your_data]
   res.send('Hello World!')
 })
 
