80e52cad03dbaef57131b8f6e14ca410aee39f33
Carvel Saputra Martaloho <carvel.saputra@gmail.com>
2021-10-19 19:24:57 +0700

    init first UI with vuetify

--- .nuxt/index.js
+++ .nuxt/index.js
@@ -12,6 +12,8 @@ import { setContext, getLocation, getRouteData, normalizeError } from './utils'
 
 /* Plugins */
 
+import nuxt_plugin_plugin_6c869087 from 'nuxt_plugin_plugin_6c869087' // Source: ./vuetify/plugin.js (mode: 'all')
+
 // Component: <ClientOnly>
 Vue.component(ClientOnly.name, ClientOnly)
 
@@ -60,7 +62,7 @@ async function createApp(ssrContext, config = {}) {
   // here we inject the router and store to all child components,
   // making them available everywhere as `this.$router` and `this.$store`.
   const app = {
-    head: {"meta":[],"link":[],"style":[],"script":[]},
+    head: {"meta":[],"link":[{"rel":"stylesheet","type":"text\u002Fcss","href":"https:\u002F\u002Ffonts.googleapis.com\u002Fcss?family=Roboto:100,300,400,500,700,900&display=swap"},{"rel":"stylesheet","type":"text\u002Fcss","href":"https:\u002F\u002Fcdn.jsdelivr.net\u002Fnpm\u002F@mdi\u002Ffont@latest\u002Fcss\u002Fmaterialdesignicons.min.css"}],"style":[],"script":[]},
 
     router,
     nuxt: {
@@ -173,6 +175,10 @@ async function createApp(ssrContext, config = {}) {
     }
   }
   // Plugin execution
+
+  if (typeof nuxt_plugin_plugin_6c869087 === 'function') {
+    await nuxt_plugin_plugin_6c869087(app.context, inject)
+  }
 
   // Lock enablePreview in context
   if (process.static && process.client) {
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,45 @@
 <template>
-  <h1>Hello world!</h1>
+  <v-app>
+    <v-app-bar app color="primary" elevate-on-scroll>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title
+        ><span class="font-weight-bold warning--text">Free</span>
+        <span class="white--text">QR-Generator</span>
+      </v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <v-sheet class="overflow-y-auto">
+          <h1>hello world</h1>
+        </v-sheet>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    drawer: null,
+  }),
+
+  props: {
+    source: String,
+  },
+};
+</script>
